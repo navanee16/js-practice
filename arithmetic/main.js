5ea54d71410f1c48efb0d15199401f65774110dd
Navanee <navaneekrish164@gmail.com>
2022-07-05 17:14:02 +0530

    arithmetic

---
+++ arithmetic/main.js
@@ -0,0 +1,19 @@
+let a=2;
+let b=3;
+let total=a+b;
+console.log(total);
+
+let x=5;
+let y=2;
+let result=x-y;
+console.log(result);
+
+let p=3;
+let q=4;
+let total1=p*q;
+console.log(total1);
+
+let s=10;
+let t=2;
+let total2=s/t;
+console.log(total2)
