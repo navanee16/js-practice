9078b6edda91b5eaf45001c49f155e77515acf8a
Navanee <navaneekrish164@gmail.com>
2022-07-29 11:58:39 +0530

    js-practice

---
+++ getelementbyid/main.js
@@ -0,0 +1,17 @@
+
+ function a() {
+  let h = document.getElementById("h");
+    var text = h.innerHTML;
+    x = "navanee";
+
+    console.log(text);
+  h.innerHTML = text+ " " + x;
+ 
+
+ 
+    
+ 
+  
+ 
+
+
